eb6c94833d806b1832b41e774930823fd9b99fac
Peter <peterhsaxton@gmail.com>
2014-03-22 11:14:41 +0000

    rectified tests for error messages

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -6,17 +6,17 @@ describe Github do
 
   it 'should be initialized with a username' do
     expect(user.username).to eq('CrowdHailer')
-    expect(user).to be_valid
+    expect(user).not_to be_valid
   end
 
   it 'should require a username' do
     expect(empty_user).not_to be_valid
-    expect(empty_user.errors.on(:username)).to eq([])
+    expect(empty_user.errors.on(:username)).not_to be_empty
   end
 
   it 'should require a users stats uri' do
     expect(empty_user).not_to be_valid
-    expect(empty_user.errors.on(:stats_uri)).to eq([])
+    expect(empty_user.errors.on(:stats_uri)).not_to be_empty
   end
 
   it 'should return a users api uri' do
