e377d49dd7eea742a41716ecf31e9dcd738c6969
Peter <peterhsaxton@gmail.com>
2014-03-22 21:55:25 +0000

    User created invalid

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -2,19 +2,28 @@ require 'spec_helper'
 require './github'
 
 describe Github do
-  let(:user) { Github.new(username: 'CrowdHailer') }
-  let(:empty_user) { Github.new }
 
   before(:each) do
     stub_request(:any, STUB_PATHS[:github_user_profile])
       .to_return(File.new('spec/fixtures/github_user_profile.txt'))
   end
 
-  [:username, :repos_url, :cv_url].each do |attribute|
-    it "it should require a #{attribute}" do
-      expect(empty_user).not_to be_valid
-      expect(empty_user.errors.on(attribute)).not_to be_empty
+  context 'Unpopulated github user' do
+    let(:user) { Github.new }
+    [:username, :repos_url, :cv_url].each do |attribute|
+      it "it should require a #{attribute}" do
+        expect(user).not_to be_valid
+        expect(user.errors.on(attribute)).not_to be_empty
+      end
     end
+  end
+
+  context 'User github routes defined from username' do
+    let(:user) { Github.new(username: 'CrowdHailer') }
+    it 'should be created invalid' do
+      expect(user).not_to be_valid
+    end
+
   end
 
 end
