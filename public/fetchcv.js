1dd9f0995d8e1d0fcf52d4184f618d4e113cd789
rolando barbella <rolandobarbella@gmail.com>
2014-03-24 15:43:36 +0000

    Append cv data to target

--- public/fetchcv.js
+++ public/fetchcv.js
@@ -1,4 +1,5 @@
 console.log('hello');
-$.get('http://www.lvh.me:5000/', function(data) {
+$.get('http://www.lvh.me:5000/Crowdhailer', function(data) {
 	console.log(data);
+  $('#cv-service-container').append(data);
 });
